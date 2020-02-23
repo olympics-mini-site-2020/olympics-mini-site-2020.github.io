5b34c025cb0511b0fd08fc31c08ca56a07654b19
Leckie <leckie.chan@gmail.com>
2020-02-23 19:56:56 +0800

    keyword

--- javascripts/amnesty.js
+++ javascripts/amnesty.js
@@ -1,124 +1,71 @@
-(function (global) {
-	var defaultLang= "en";
-	var supportedLanguages = ['en', 'es', 'fr', 'zh_Hans', 'zh_Hant', 'zh_TW'];
-	var lang = getLangFromQueryString();
-	var dir;
-	setLangAndDir(lang);
-	var dictionary;
+(function(global) {
+	var globeContainer = document.getElementById('globe');
+	function setupGlobe(option) {
+		var fullSize = 840,
+			ballSize = 575,
+			velocity = 0.0025,
+			dragX = 220;
 
-	function Dictionary(dictionaryJson) {
-		this.dictionary = dictionaryJson;
+		var front = d3.geoOrthographic()
+			.translate([ballSize / 2, ballSize / 2])
+			.scale(ballSize / 2)
+			//.clipAngle(90)
+			//.rotate([0, 0])
+			.precision(.1);
+		var frontPath = d3.geoPath().projection(front);
+		var globe = d3.select(globeContainer);
+		var globeSvg = globe.append('svg')
+			.attr('id', 'globeSvg')
+			.attr('viewBox', '0 0 ' + fullSize + ' ' + fullSize);
+		var ball = globeSvg.append('g')
+			.attr('width', ballSize)
+			.attr('height', ballSize)
+			.attr('class', 'ball')
+			.attr('transform', 'translate(' + (fullSize / 2 - ballSize / 2) + ', ' + (fullSize / 2 - ballSize / 2) + ')');
+		ball.append('path').datum(d3.geoGraticule().step([30, 30])).attr('class', 'graticule');
+		ball.append('path').datum({ type: 'Sphere' }).attr('class', 'outline');
+
+		var globePath = ball.selectAll('path');
+
+		globeContainer.step = (function(velocity, front, frontPath, globePath) {
+			return function(elapsed) {
+				//console.log(velocity * elapsed);
+				front.rotate([dragX + velocity * elapsed, -35, -25]);
+				globePath.attr('d', frontPath);
+			};
+		})(velocity, front, frontPath, globePath);
+		/*if(option != 'static')
+			globeContainer.timer = d3.timer(globeContainer.step);
+		else */globeContainer.step(0);
+
+		// d3.json('data/data.json', function(error, data) {
+
+		// });
+		var keyWords = new Array('LGBT', 'Human', 'Liberation', 'Diversity', 'Solidarity', 'Sportsmanship', 'Athlete', 'Minority', 'Equal/equality', 'Discrimination', 'Harmony', 'Warmth', 'Fair/fairness', 'Self', 'Pride', 'Rainbow', 'Colours', 'Voice', 'Love', 'Glory', 'Brave', 'Natural', 'Beauty', 'Tolerance', 'Motion', 'Identity', 'Respect', 'Free/freedom', 'Inclusive', 'Connect');
+		var group = globeSvg.append('g')
+			.attr('class', 'keywords')
+			.attr('transform', 'translate(' + fullSize / 2 + ', ' + fullSize / 2 + ')');
+
+		var r = d3.scaleLinear()
+            .domain([0,keyWords.length])
+            .range([0,360]);
+		var y = d3.scaleLinear()
+            .domain([0, 100])
+            .range([ballSize / 4, 0]);
+
+		group.selectAll('.keyword')
+			.data(keyWords)
+			.enter()
+			.append('g')
+			.attr('transform', function(d, i) { return 'rotate(' + ((360 / keyWords.length) * i) + ')'; })
+			// .attr('transform', function(d, i) { return 'rotate(' + r(i) + ')'; })
+			.attr('class', 'keyword')
+			.append('text')
+			.attr('text-anchor', 'start')
+			.attr('x', 300)
+			.attr('y', function(d) { return y(90); })
+			.attr('class', 'head')
+			.text(function(d) { return d; });
 	}
-
-	Dictionary.prototype.getTranslation = function (key, language) {
-		var translation = '';
-
-		if (language == undefined) {
-			language = lang;
-		}
-
-		if ( this.dictionary.hasOwnProperty(key) && this.dictionary[key].hasOwnProperty(language) ) {
-			translation = this.dictionary[key][language];
-		}
-
-		else {
-			translation = key;
-		}
-
-		return translation;
-	};
-
-	function getLangFromQueryString(){
-		// Mostly shamelessly cribbed from here: http://stackoverflow.com/a/901144/20578
-		var lang = defaultLang;
-		var regex = new RegExp("[?&]" + "lang" + "(=([^&#]*)|&|#|$)");
-		var results = regex.exec(window.location.href);
-
-		if (results && results[2]) {
-			lang = decodeURIComponent(results[2].replace(/\+/g, " "));
-		}
-
-		return lang;
-	}
-
-	function setLangAndDir(lang) {
-		var htmlEl = document.getElementsByTagName("html")[0];
-
-		if (supportedLanguages.indexOf(lang) > -1) {
-			htmlEl.lang = lang;
-		}
-
-		if (lang === "ar") {
-			htmlEl.dir = "rtl";
-			dir = "rtl";
-		}
-		else {
-			dir = "ltr";
-		}
-	}
-
-	function translateHTML() {
-		var el,
-				translateTextEls = document.querySelectorAll('[data-translate]'),
-				translateTitleEls = document.querySelectorAll('[data-translate-title]'),
-				i;
-
-		for (i=0; i<translateTextEls.length; i++) {
-			el = translateTextEls[i];
-			el.innerHTML = dictionary.getTranslation( el.getAttribute('data-translate') );
-		}
-
-		for (i=0; i<translateTitleEls.length; i++) {
-			el = translateTitleEls[i];
-			el.title = dictionary.getTranslation( el.getAttribute('data-translate-title') );
-		}
-	}
-
-	function removeLoadingScreen() {
-		var loadingScreenEl = document.getElementById('loading');
-
-		if (!loadingScreenEl) {
-			return;
-		}
-
-		if(typeof loadingScreenEl.style['transition'] !== 'undefined') {
-			loadingScreenEl && loadingScreenEl.addEventListener('transitionend', function () {
-				loadingScreenEl.parentNode.removeChild(loadingScreenEl);
-			});
-
-			loadingScreenEl.style.opacity = '0';
-		}
-
-		else {
-			loadingScreenEl.parentNode.removeChild(loadingScreenEl);
-		}
-	}
-
-	//Loads in the world data, the active countries, and the translation dictionary
-	// queue()
-	// 		.defer(d3.json, "data/world-topo-1-3.json?cachebust="+(+new Date()))
-	// 		.defer(d3.json, "data/data.json?cachebust="+(+new Date()))
-	// 		.defer(d3.json, "lang/dictionary.json?cachebust="+(+new Date()))
-	// 		.await(ready);
-
-	function ready(error, world, active, dict) {
-		dictionary = new Dictionary(dict);
-		translateHTML();
-
-		if ('parentIFrame' in window) {
-				parentIFrame.size();
-		}
-		removeLoadingScreen();
-	}
-
-	var throttleTimer;
-	function throttle() {
-		window.clearTimeout(throttleTimer);
-			throttleTimer = window.setTimeout(function() {
-				if ('parentIFrame' in window) {
-						parentIFrame.size();
-				}
-			}, 200);
-	}
-
+	setupGlobe();
 })(window);
