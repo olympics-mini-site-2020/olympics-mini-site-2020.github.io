47d07f78408cd488ecca4678f2b6deb9109606be
Leckie <leckie.chan@gmail.com>
2020-02-29 19:28:05 +0800

    Static

--- scripts/copy-vendor-js.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
- 
-cp node_modules/iframe-resizer/js/iframeResizer.contentWindow.js src/javascripts/vendor/iframeResizer.contentWindow.js;
-cp node_modules/iframe-resizer/js/iframeResizer.js src/javascripts/vendor-external/iframeResizer.js;
